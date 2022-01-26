83728f97baebc3212dd7c68f3d1ad25937eaa629
Marc Anguera Insa <srmarc.ai@gmail.com>
2022-01-26 01:30:14 +0100

    use css3 vars + config tweaks

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,12 @@ module.exports = {
       {
         test: /\.m?js$/,
         exclude: /(node_modules)/,
-        loader: 'babel-loader'
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
       }
     ]
   }
